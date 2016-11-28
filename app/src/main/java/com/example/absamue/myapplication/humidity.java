3b6776a48b4b94c8f1bb6a2227976e8e15d83f49
Andrew Samuels <absamue@g.clemson.edu>
2016-11-28 16:55:49 -0500

    added rest of graphs

--- app/src/main/java/com/example/absamue/myapplication/humidity.java
+++ app/src/main/java/com/example/absamue/myapplication/humidity.java
@@ -10,7 +10,14 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class humidity extends AppCompatActivity implements SensorEventListener {
     private SensorManager mSensorManager;
@@ -18,6 +25,11 @@ public class humidity extends AppCompatActivity implements SensorEventListener {
     private ListView listview;
     private ArrayList<String> data = new ArrayList();
     private ArrayAdapter adapter;
+    private GraphView graph;
+    private LineGraphSeries<DataPoint> series;
+    private int current = 0;
+    private float current_data;
+    private Timer mTimer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +46,41 @@ public class humidity extends AppCompatActivity implements SensorEventListener {
         listview = (ListView) findViewById(R.id.humidity_listview);
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
         listview.setAdapter(adapter);
+
+        //set up graph
+        graph = (GraphView) findViewById(R.id.humidity_graph);
+        series = new LineGraphSeries<>();
+        series.setDrawDataPoints(true);
+        graph.addSeries(series);
+
+        //set axis labels
+        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // show normal x values
+                    return super.formatLabel(value, isValueX) + "s";
+                } else {
+                    // show lx for y values
+                    return super.formatLabel(value, isValueX) + "%";
+                }
+            }
+        });
+
+        //set bounds of graph
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(10);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         data.clear();
         data.add(Float.toString(sensorEvent.values[0]) + "%");
+
+        //get data for use in graph
+        current_data = sensorEvent.values[0];
+
         adapter.notifyDataSetChanged();
     }
 
@@ -52,6 +93,16 @@ public class humidity extends AppCompatActivity implements SensorEventListener {
     protected void onResume(){
         super.onResume();
         mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //update graph one a second
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                series.appendData(new DataPoint(current, current_data), true, 10);
+                current += 1;
+
+            }
+        }, 1000, 1000);
     }
 
     @Override
